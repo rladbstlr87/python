927142ff4b844a8af2b9c7ed73df7e374ce56fe0
rladbstlr87 <manylong@naver.com>
2025-02-18 14:59:32 +0900

    module & package

---
+++ myPackage/math/fomula.py
@@ -0,0 +1,14 @@
+def fact(n):
+    if n == 1:
+        return 1
+    else:
+        return fact(n-1) * n
+
+def my_max(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+pi = 3.14
+
